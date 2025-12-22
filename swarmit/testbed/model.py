bdd857ae6f3381cffc481f796de7a47954917a6f
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-12-22 16:27:54 +0100

    swarmit/testbed/webserver: refactor and extend tests

--- swarmit/testbed/model.py
+++ swarmit/testbed/model.py
@@ -9,13 +9,9 @@ from sqlalchemy import (
     create_engine,
     text,
 )
-from sqlalchemy.engine import Connection
+from sqlalchemy.engine import Connection, Engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./.data/database.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
@@ -36,69 +32,58 @@ class AwareDateTime(TypeDecorator):
 
 class JWTRecord(Base):
     __tablename__ = "jwt_records"
-    id = Column(Integer, primary_key=True, index=True)
+
+    id_ = Column(Integer, primary_key=True, index=True)
     jwt = Column(String, unique=True, nullable=False)
     date_start = Column(AwareDateTime, nullable=False)
     date_end = Column(AwareDateTime, nullable=False)
 
 
-Base.metadata.create_all(bind=engine)
+def create_db_engine(url: str) -> Engine:
+    return create_engine(url, connect_args={"check_same_thread": False})
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def create_session_factory(engine: Engine):
+    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def create_prevent_overlap_trigger(conn: Connection):
     conn.execute(
         text(
             """
-    CREATE TRIGGER IF NOT EXISTS prevent_overlap
-    BEFORE INSERT ON jwt_records
-    FOR EACH ROW
-    BEGIN
-        SELECT
-        CASE
-            WHEN EXISTS (
-                SELECT 1 FROM jwt_records
-                WHERE NEW.date_start < date_end
-                AND NEW.date_end > date_start
-            )
-            THEN
-                RAISE (ABORT, 'Overlapping date range detected')
+        CREATE TRIGGER IF NOT EXISTS prevent_overlap
+        BEFORE INSERT ON jwt_records
+        FOR EACH ROW
+        BEGIN
+            SELECT CASE
+                WHEN EXISTS (
+                    SELECT 1 FROM jwt_records
+                    WHERE NEW.date_start < date_end
+                    AND NEW.date_end > date_start
+                )
+                THEN RAISE (ABORT, 'Overlapping date range detected')
+            END;
         END;
-    END;
     """
         )
     )
     conn.execute(
         text(
             """
-    CREATE TRIGGER IF NOT EXISTS prevent_overlap_update
-    BEFORE UPDATE ON jwt_records
-    FOR EACH ROW
-    BEGIN
-        SELECT
-        CASE
-            WHEN EXISTS (
-                SELECT 1 FROM jwt_records
-                WHERE id != OLD.id
-                AND NEW.date_start < date_end
-                AND NEW.date_end > date_start
-            )
-            THEN
-                RAISE (ABORT, 'Overlapping date range detected')
+        CREATE TRIGGER IF NOT EXISTS prevent_overlap_update
+        BEFORE UPDATE ON jwt_records
+        FOR EACH ROW
+        BEGIN
+            SELECT CASE
+                WHEN EXISTS (
+                    SELECT 1 FROM jwt_records
+                    WHERE id_ != OLD.id_
+                    AND NEW.date_start < date_end
+                    AND NEW.date_end > date_start
+                )
+                THEN RAISE (ABORT, 'Overlapping date range detected')
+            END;
         END;
-    END;
     """
         )
     )
-
-
-# Run trigger creation
-with engine.connect() as conn:
-    create_prevent_overlap_trigger(conn)
